bbe214778e1c9b73af799e352249fd698cb4d7c2
AnilKumar <anilattada222@gmail.com>
2026-03-06 19:18:50 +0530

    ROBOSHOP added

--- roboshop.sh
+++ roboshop.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-06f23152b42979b41"  
 INSTANCES=("mongodb" "frontend" "catalogue")
@@ -10,10 +9,7 @@ DOMAIN_NAME="anilkumar.shop"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro \
-    --security-group-ids sg-06f23152b42979b41 \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" \
-    --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro  --security-group-ids sg-06f23152b42979b41  --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances--instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
