37968f221e2b7216837106fede6567da21357ab3
AnilKumar <anilattada222@gmail.com>
2026-03-10 14:52:07 +0530

    ROBOSHOP added

---
+++ common.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)"  | tee -a $LOG_FILE
+
+check_root(){
+    if [ $USERID -ne 0 ]
+    then 
+        echo -e "$R ERROR :: run with root access $N" | tee -a $LOG_FILE
+        exit 1
+    else 
+        echo -e "$G running with root access $N" | tee -a $LOG_FILE
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE
+    else    
+    echo -e "$2 ..... $R FAILURE $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+print_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "Script executed successfully, $Y time taken is: $TIME_TAKEN seconds $N"
+}
--- mongodb.sh
+++ mongodb.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
+source ./common.sh
+app_name=mongodb
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)"  | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR :: run with root access $N" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo -e "$G running with root access $N" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE
-    else    
-    echo -e "$2 ..... $R FAILURE $N" | tee -a $LOG_FILE
-    fi
-
-}
+check_root
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying mongodb repo"
@@ -48,3 +22,5 @@ VALIDATE $? "Editing Mongod conf file"
 
 systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting Mongodb"
+
+print_time
