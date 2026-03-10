9ec24cea517d1ff9dd46ee4021d3b5aebbcd59bc
AnilKumar <anilattada222@gmail.com>
2026-03-10 15:40:08 +0530

    ROBOSHOP added

--- frontend.sh
+++ frontend.sh
@@ -1,36 +1,7 @@
 #!/bin/bash
 
-USERID=$(id -u)
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)"  | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR :: run with root access $N" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo -e "$G running with root access $N" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE
-    else    
-    echo -e "$2 ..... $R FAILURE $N" | tee -a $LOG_FILE
-    fi
-
-}
+source ./common.sh
+check_root
 
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabiling default nginx"
@@ -63,3 +34,5 @@ VALIDATE $? "Copying nginx conf"
 
 systemctl restart nginx &>>$LOG_FILE
 VALIDATE $? "Restarting nginx"
+
+print_time
