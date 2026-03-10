47bc57747ca69d70b31bf45e8db011ea70a255b9
AnilKumar <anilattada222@gmail.com>
2026-03-10 12:23:15 +0530

    ROBOSHOP added

---
+++ mysql.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)"  | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR :: run with root access $N" | tee -a $LOG_FILE
+    exit 1
+else 
+    echo -e "$G running with root access $N" | tee -a $LOG_FILE
+fi
+
+echo "Please enter root password to setup"
+read -s MYSQL-ROOT-PASSWORD
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE
+    else    
+    echo -e "$2 ..... $R FAILURE $N" | tee -a $LOG_FILE
+    fi
+
+}
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing mysql server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabling mysql"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Starting mysql"
+
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+VALIDATE $? "Setting mysql root passsword"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
