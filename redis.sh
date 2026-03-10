3f0645a20c9067bb65ccb59bfcb7422c224c97c1
AnilKumar <anilattada222@gmail.com>
2026-03-10 15:36:03 +0530

    ROBOSHOP added

--- redis.sh
+++ redis.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
+source ./common.sh
+app_name=redis
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)"  | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R ERROR :: run with root access $N" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo -e "$G running with root access $N" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOG_FILE
-    else    
-    echo -e "$2 ..... $R FAILURE $N" | tee -a $LOG_FILE
-    fi
-
-}
+check_root
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disabiling redis"
@@ -50,7 +23,4 @@ VALIDATE $? "Enabiling redis"
 systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting redis"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
